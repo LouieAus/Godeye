20e4d64bd699c6fe14213140c4d18fce1403bc0b
LouieAus <120642016+LouieAus@users.noreply.github.com>
2024-06-23 11:34:31 +0200

    Json parser added

--- json_parser.cpp
+++ json_parser.cpp
@@ -100,4 +100,193 @@ namespace json
 
 		return new_update;
 	}
+
+	//std::vector<vk::User> json::ParseToVkUser(std::string text)
+	//{
+	//	// TODO
+	//}
+
+	std::string GetSubJson(int index, std::string& text, int& exit_index)
+	{
+		std::string result = "";
+
+		int level = 0;
+		for (auto i = index; i != text.size(); i++)
+		{
+			if (text[i] == '{')
+				level++;
+			else if (text[i] == '}')
+				level--;
+
+			result += text[i];
+
+			if (level == 0)
+			{
+				exit_index = i;
+				break;
+			}
+		}
+		
+		return result;
+	}
+
+	std::vector<DataPair> json::ParseToJson(std::string text)
+	{
+		std::vector<DataPair> result;
+
+		text = text.substr(1, text.size() - 2);
+		
+		int state = 0;
+		std::string id = "";
+		std::string value = "";
+		int exit_index = 0;
+
+		for (auto i = 0; i != text.size(); i++)
+		{
+			if (state == 0)
+			{
+				if (text[i] == '"')
+					state = 1;
+			}
+			else if (state == 1)
+			{
+				if (text[i] == '"')
+					state = 2;
+				else
+					id += text[i];
+			}
+			else if (state == 2)
+			{
+				if (text[i] == ':')
+					state = 3;
+			}
+			else if (state == 3)
+			{
+				if (text[i] == '"')
+					state = 10;
+				else if (text[i] == '{')
+					state = 20;
+				else if (text[i] == '[')
+					state = 30;
+				else
+					state = 40;
+			}
+			else if (state == 4)
+			{
+				if (i == exit_index)
+					state = 0;
+			}
+			else if (state == 10)
+			{
+				if (text[i] == '"')
+				{
+					value = '"' + value + '"';
+					DataPair data = { id, value };
+					result.push_back(data);
+					id = "";
+					value = "";
+					state = 0;
+				}
+				else
+				{
+					value += text[i];
+				}
+			}
+			else if (state == 30)
+			{
+				if (text[i] != ']')
+				{
+					std::vector<std::vector<DataPair>> vec;
+					exit_index = i - 2;
+					while (text[exit_index + 1] != ']')
+					{
+						std::string sub = GetSubJson(exit_index + 2, text, exit_index);
+						std::vector<DataPair> subresult = ParseToJson(sub);
+						vec.push_back(subresult);
+					}
+					DataPair data = { id, vec };
+					result.push_back(data);
+					id = "";
+					value = "";
+					state = 4;
+				}
+				else
+				{
+					DataPair data = { id, "" };
+					result.push_back(data);
+					id = "";
+					value = "";
+					state = 0;
+				}
+			}
+
+			if (state == 20)
+			{
+				std::string sub = GetSubJson(i, text, exit_index);
+				std::vector<DataPair> subresult = ParseToJson(sub);
+				DataPair data = { id, subresult };
+				result.push_back(data);
+				id = "";
+				value = "";
+				state = 4;
+			}
+			else if (state == 40)
+			{
+				if (text[i] == ',' || text[i] == '}' || i == text.size() - 1)
+				{
+					DataPair data = { id, value };
+					result.push_back(data);
+					id = "";
+					value = "";
+					state = 0;
+				}
+				else
+				{
+					value += text[i];
+				}
+			}
+		}
+
+		return result;
+	}
+
+	DataPair json::GetData(std::vector<int> ids, std::vector<DataPair>& vec)
+	{
+
+	}
+
+	void PrintDataPair(std::vector<DataPair>& vec, int level)
+	{
+		for (DataPair& data : vec)
+		{
+			for (int i = 0; i != level; i++)
+				std::cout << '\t';
+
+			std::cout << data.id << ':';
+			try
+			{
+				std::string value = std::get<std::string>(data.value);
+				std::cout << value << '\n';
+			}
+			catch (const std::bad_variant_access& ex)
+			{
+				try
+				{
+					std::cout << '\n';
+					std::vector<DataPair> pair = std::get<std::vector<DataPair>>(data.value);
+					PrintDataPair(pair, level + 1);
+				}
+				catch (const std::bad_variant_access& ex)
+				{
+					std::cout << '\n';
+					std::vector<std::vector<DataPair>> vec = std::get<std::vector<std::vector<DataPair>>>(data.value);
+					for (auto i = 0; i != vec.size(); i++)
+					{
+						PrintDataPair(vec[i], level + 1);
+						std::cout << '\n';
+					}
+				}
+			}
+		}
+	}
 }
--- json_parser.h
+++ json_parser.h
@@ -1,11 +1,24 @@
 #pragma once
-#include <iostream>
-#include <variant>
 #include <string>
-#include <vector>
+#include <variant>
 #include "tg_types.h"
+#include "api_vk.h"
 
 namespace json
 {
 	tg::Update ParseToUpdate(std::string text);
+
+	//std::vector<vk::User> ParseToVkUser(std::string text);
+
+	struct DataPair
+	{
+		std::string id;
+		std::variant<std::string, std::vector<DataPair>, std::vector<std::vector<DataPair>>> value;
+	};
+
+	std::vector<DataPair> ParseToJson(std::string text);
+
+	DataPair GetData(std::vector<int> ids, std::vector<DataPair>& vec);
+
+	void PrintDataPair(std::vector<DataPair>& vec, int level);
 }
